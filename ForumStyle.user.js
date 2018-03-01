50af2d408f2503d3953a3ff303a0c2d857664213
Ondřej Kotas <ok@krtkovo.eu>
2018-03-01 22:01:40 +0100

    Create ForumStyle.user.js

---
+++ ForumStyle.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Pirati phpBB style
+// @namespace    http://pirati.cz/
+// @version      0.1
+// @description  Extention for Stylish script on forum.pirati.cz
+// @author       Ondrej Kotas
+// @match        https://forum.pirati.cz/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+$("a.lastsubject").each(function() {
+  $( this ).text($( this).attr("title"));
+});
+
+$("#nav-main li.rightside:not(:first)").each(function() {
+    var prevLeft = parseFloat($( this ).prev().css("left").replace(/[^-\d\.]/g, ''));
+    prevLeft = prevLeft || 0;
+    var prevWidth = $ ( this ).prev().width();
+    $( this ).css("left", prevLeft + prevWidth + 10);
+});
+
+var newTopics = $("#nav-main li.responsive-hide:contains('Nové')");
+newTopics.html(newTopics.html().replace("Nové", "Nového"));
+
+var activeTopics = $("#nav-main li.responsive-hide:first");
+activeTopics.parent().append(activeTopics);
+
+
+var navMain = $("#nav-main");
+
+var logout = $("#username_logged_in a[title='Odhlásit se']").clone();
+logout.attr("id", "ok_logoutButton");
+navMain.append(logout);
+
+var modPanel = $("#quick-links a[title='Moderátorský panel']").clone();
+modPanel.attr("id", "ok_modPanel");
+navMain.append(modPanel);
+
+var thanksList = $("#quick-links a[href='/app.php/thankslist']").clone();
+thanksList.attr("id", "ok_thanksList");
+navMain.append(thanksList);
+
+
+var groupsList = thanksList.clone();
+groupsList.find("span").text("Nastavit zobrazovaná fóra");
+groupsList.attr("href", "ucp.php?i=167");
+groupsList.attr("id", "ok_groupsList");
+groupsList.find("i").addClass("fa-user-plus").removeClass("fa-thumbs-o-up");
+navMain.append(groupsList);
+console.log("testik");
