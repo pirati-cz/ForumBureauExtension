824570e1f89c0f3b89db9c627a15e64e4ed38b89
Ondřej Kotas <ok@krtkovo.eu>
2018-03-02 04:51:21 +0100

    Update ForumBureauExtension.Templates.user.js

--- ForumBureauExtension.Templates.user.js
+++ ForumBureauExtension.Templates.user.js
@@ -1,70 +1,151 @@
 // ==UserScript==
-// @name         Forum Bureau Extension - Templates
+// @name         Forum Bureau extension - Templates
 // @namespace    http://pirati.cz/
-// @version      0.1
-// @description  Extentions for forum.pirati.cz
+// @version      1.0
+// @description  Extention for Stylish script on forum.pirati.cz
 // @author       Ondrej Kotas
 // @match        https://forum.pirati.cz/posting.php?mode=post*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require      https://raw.githubusercontent.com/ether/etherpad-lite-jquery-plugin/master/js/etherpad.js
 // @grant        none
 // ==/UserScript==
 
-$(function () {
-    // === Define all elements ===    
-    // Bureau blocks
-    var bureau_block = $("<div id=\"bureau-block\"></div>");
-    bureau_block.css("border-radius", "5px");
-    bureau_block.css("margin", "10px 0 10px 0");
-    bureau_block.css("padding", "0");
-    bureau_block.css("width", "100%");
-    bureau_block.css("min-height", "100px");
-    bureau_block.css("background", "#cadceb");
-    
-    var bureau_content = $("<div id=\"bureau-content\"></div>");
-    bureau_content.css("margin", "0 10px 5px 10px");
-    
-    // Bureau texts
-    var bureau_title = $("<h1>Šablony</h1>");
-    bureau_title.css("color", "#105289");
-    bureau_title.css("margin", "0 10px 0 10px");
-    bureau_title.css("padding", "10px 0 5px 0");
-    
-    var bureau_titleLine = $("<hr />");
-    bureau_titleLine.css("color", "#ffffff");
-    bureau_titleLine.css("border-color", "#ffffff");
-    bureau_titleLine.css("width", "100%");
-    
-    var bureau_cats = [
-        $("<h2 class=\"bureau\">Schůze</h2>"),
-        $("<h2 class=\"bureau\">Jednání</h2>")
-    ];
+var DEBUG = true;
 
-    var bureau_cats_links = [
-        $("<ul class=\"bureau\" id=\"cat0\"></ul>"),
-        $("<ul class=\"bureau\" id=\"cat1\"></ul>")
-    ];
-
-    var index;
-    for (index = 0; index < bureau_cats.length; ++index) {
-        bureau_cats[index].css("color", "#000000");
-        bureau_cats[index].css("margin", "0");
-        bureau_cats[index].css("padding", "0");
-        bureau_cats[index].css("font-size", "10pt");
-        bureau_cats[index].css("font-weight", "bold");
-
-        bureau_cats_blocks[index].css("float:", "");
+function FillTemplatesList(data) {
+  var templateBox = $("#postform #postingbox").find("dl:contains('Předmět:')").clone();
+  var templateListBox = $("<select></select>");
+  var lines = data.split("\n");
+  var helplink = $("<a></a>");
+  var hrline = $("<hr />");
+  
+  helplink.attr("href", "https://github.com/pirati-cz/ForumBureauExtension/wiki");
+  helplink.text("[?]");
+  helplink.attr("title", "Nabídka šablon pro rutinní úlohy. Pro více informací klikněte.");
+  helplink.attr("target", "_blank");
+  
+  templateBox.find("label").text("Šablona:");
+  templateBox.find("label").append(helplink);
+  templateBox.find("input").remove();
+  templateListBox.css("min-width", "335px");
+  templateListBox.append($("<option />").val("").text(""));  
+  templateListBox.attr("id", "bureau_select");
+  
+  templateListBox.on("change", function(){
+     if(this.value != "") {
+        $.get(this.value + "/export/txt", FillPostingboxWithTemplate);
+     }
+     else {
+        $("#postingbox #subject").val("");
+        $("#postingbox textarea").text("");
+     }
+  });
+  
+  for (i = 0; i < lines.length; i++) {
+    if(lines[i] != "") {
+      var pair = lines[i].split("|");
+  
+      Log("INFO", pair);
+      templateListBox.append($("<option />").val(pair[1]).text(pair[0])); 
     }
+  }
+  
+  templateBox.append(templateListBox);
+  templateBox.append(hrline);
+  $("#postingbox dl").prepend(templateBox);
+}
+
+function FillPostingboxWithTemplate(data) {
+  var lines = data.split("\n");
+  if(lines[0] == "#šablona") {
+     // TODO: syntax engine na formuláře
+     // 
+     if(SearchStringInArray("!anketa", lines) > 0) {
+       FillValWithPadTag("#poll_title", "!anketa-otazka:", lines);
+       FillTextWithPadTag("#poll_option_text", "!anketa-moznosti:", lines);
+       FillNumValWithPadTag("#poll_max_options", "!anketa-max-moznosti:", lines);
+       FillNumValWithPadTag("#poll_length", "!anketa-delka-trvani:", lines);
+       FillCheckboxWithPadTag("#poll_vote_change", "!anketa-povolit-zmenu-hlasu:", lines);
+       FillCheckboxWithPadTag("#poll_show_results", "!anketa-zobrazit-vysledky:", lines);
+     }
     
+     FillValWithPadTag("#postingbox #subject", "!predmet:", lines);
+     FillTextWithPadTag("#postingbox textarea", "!obsah:", lines);
+  }
+  else {
+     $("#postingbox #subject").val(lines[0]); 
+     $("#postingbox textarea").val(lines.join("\n"));
+     Log("INFO", lines)
+  }
+}
 
-    // === Compose feature ===
-    for (index = 0; index < bureau_cats.length; ++index) {
-        bureau_cats[index].append(bureau_cats_blocks[index]);
-        bureau_content.append(bureau_cats[index]);
+function FillTextWithPadTag(element, tag, lines) {
+  if(SearchStringInArray(tag, lines) > 0) {
+    var tagClose = tag.replace("!", "").replace(":", "!");
+    var openingLine = SearchStringInArray(tag, lines) +1;
+    var closingLine = SearchStringInArray(tagClose, lines);
+    Log("INFO", lines.slice(openingLine, closingLine).join("\n"));
+    $(element).val(lines.slice(openingLine, closingLine).join("\n"));
+  }
+}
+
+function FillValWithPadTag(element, tag, lines) {
+  if(SearchStringInArray(tag, lines) > 0) {
+    var value = lines[SearchStringInArray(tag, lines)].replace(tag, "");
+    Log("INFO", value);
+    $(element).val(value);
+  }
+}
+
+function FillNumValWithPadTag(element, tag, lines) {
+  if(SearchStringInArray(tag, lines) > 0) {
+    var value = lines[SearchStringInArray(tag, lines)].replace(tag, "");
+    if(value == "") {
+      value = "0";
     }
+    Log("INFO", value);
+    $(element).val(value);
+  }
+}
 
-    $('#page-body').prepend(bureau_block);
-    bureau_title.append(bureau_titleLine);
-    bureau_block.append(bureau_title);
+function FillCheckboxWithPadTag(element, tag, lines) {
+  if(SearchStringInArray(tag, lines) > 0) {
+    var value = lines[SearchStringInArray(tag, lines)].replace(tag, "");
+    Log("INFO", value);
+    if(value == "ano") {
+      $(element).prop('checked', true); 
+    }
+    else {
+     $(element).prop('checked', false);
+    }
+  }  
+}
 
-    bureau_block.append(bureau_content);
-})();
+function SearchStringInArray(str, strArray) {
+    for (var j=0; j<strArray.length; j++) {
+        if (strArray[j].match(str)) return j;
+    }
+    return -1;
+}
+
+// For todays date;
+Date.prototype.today = function () { 
+    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+}
+
+// For the time now
+Date.prototype.timeNow = function () {
+     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+}
+
+function Log(level, message) {
+  if(DEBUG) {
+    var newDate = new Date();
+    var datetime = newDate.today() + " " + newDate.timeNow();
+    console.log(datetime + " [" + level + "] " + message);
+  }
+}
+
+
+// TRIGGER
+$.get("https://pad.pirati.cz/p/bureau_template_list/export/txt", FillTemplatesList);
