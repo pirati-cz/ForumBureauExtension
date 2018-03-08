4c90360b0483d372af8b342c1c595a8074528d46
Ondřej Kotas <ondrej.kotas@solarwinds.com>
2018-03-08 07:07:23 +0100

    increase version due to changes

--- ForumBureauExtension.Templates.user.js
+++ ForumBureauExtension.Templates.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Forum Bureau extension - Templates
 // @namespace    http://pirati.cz/
-// @version      1.3.0.6
+// @version      1.3.0.7
 // @description  Extention for Stylish script on forum.pirati.cz
 // @author       Ondrej Kotas
 // @match        https://forum.pirati.cz/posting.php?mode=post*
