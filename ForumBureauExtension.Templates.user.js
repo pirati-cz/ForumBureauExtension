91cf95e41c2dbb088819f4292e173616d51e40ae
Ondřej Kotas <ondrej.kotas@solarwinds.com>
2018-03-14 14:20:52 +0100

    Komentáře do kódu

--- ForumBureauExtension.Templates.user.js
+++ ForumBureauExtension.Templates.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Forum Bureau extension - Templates
 // @namespace    http://pirati.cz/
-// @version      1.3.0.9
+// @version      1.3.1.0
 // @description  Extention for Stylish script on forum.pirati.cz
 // @author       Ondrej Kotas
 // @match        https://forum.pirati.cz/posting.php?mode=post*
@@ -9,60 +9,68 @@
 // @grant        none
 // ==/UserScript==
 
-var DEBUG = true;
+var DEBUG = false; // nastav na true, pokud chceš v konzoli prohlížeče vidět debug hlášky
 
 /* GLOBAL VARIABLES */
-var inputRow = $("#postform #postingbox").find("dl:contains('Předmět:')").clone();
+var inputRow = $("#postform #postingbox").find("dl:contains('Předmět:')").clone(); // vycházíme z řádku "předmět"
 
 /* TRIGGER */
 ComposeTemplateBlock();
-$.get("https://pad.pirati.cz/p/bureau_template_list/export/txt", LoadTemplatesList);
+$.get("https://pad.pirati.cz/p/bureau_template_list/export/txt", LoadTemplatesList); // stahni textový soubor z padu se seznamem šablon
 
 /* FUNCTIONS */
+
+// Poskládat blok s nabídkou šablon
 function ComposeTemplateBlock() {
-  var templateBox = inputRow.clone();
-  var templateListBox = $("<select></select>");
+  var templateBox = inputRow.clone(); // naklonujeme si řádek "předmět"
+  var templateListBox = $("<select></select>"); // vytvoříme nové objekty
   var helplink = $("<a></a>");
   var hrline = $("<hr />");
 
+  // nastavíme obsah odkazu s nápovědou (link na wiki)
   helplink.attr("href", "https://github.com/pirati-cz/ForumBureauExtension/wiki");
   helplink.text("[?]");
   helplink.attr("title", "Nabídka šablon pro rutinní úlohy. Pro více informací klikněte.");
   helplink.attr("target", "_blank");
   helplink.attr("id", "bureau_helplink");
 
-  templateBox.find("label").text("Šablona:");
-  templateBox.find("label").append(helplink);
-  templateBox.find("input").remove();
-  templateListBox.append($("<option />").val("").text("bez šablony"));  
-  templateListBox.attr("id", "bureau_select");
+  templateBox.find("label").text("Šablona:"); // přepíšeme naklonované políčko "předmět" na "šablona"
+  templateBox.find("label").append(helplink); // přidáme odkaz na wiki
+  templateBox.find("input").remove(); // odstraníme z klonu textové pole pro název příspěvku
+  templateListBox.append($("<option />").val("").text("bez šablony"));  // do nového objektu Select přidáme první možnost "bez šablony"
+  templateListBox.attr("id", "bureau_select"); //nastavíme selectu ID
 
+  // akce OnChange na Select menu - loaduje šablonu nebo resetuje formulář pro příspěvek
   templateListBox.on("change", function(){
      if(this.value != "") {
         $.get(this.value + "/export/txt", FillPostingboxWithTemplate);
      }
      else {
-        $("form#postform").trigger('reset');
-        $("#bureau_templates_form").remove();
-        ResetForm();
+        $("form#postform").trigger('reset'); // proveď úplný reset formuláře
+        $("#bureau_templates_form").remove(); // odstraň blok se šablonou
+        ResetForm(); // Odebereme anketu a resetujeme panely
      }
   });
 
-  templateBox.append(templateListBox);
-  templateBox.append(hrline);
-  $("#postingbox dl:contains('Předmět:')").prepend(templateBox);
+  templateBox.append(templateListBox); // přidat Select objekt do řádku "šablona"
+  templateBox.append(hrline); // přidat oddělovací čáru po "šablony"
+  $("#postingbox dl:contains('Předmět:')").prepend(templateBox); // to celé přidat před řádek "předmět"
 }
  
+// Loaduje seznam všech šablon z textového souboru
 function LoadTemplatesList(data) {
-  var lines = data.split("\n");
+  var lines = data.split("\n"); // každý řádek souboru jako prvek seznamu
   
   for (i = 0; i < lines.length; i++) {
+    // pokud řádek není prázdný (ty fungují jen jnako oddělovače)
     if(lines[i] != "") {
+      // pokud řádek začíná symbolem '#', použije se hodnota jako název kategorie
       if(lines[i].startsWith("#")) {
         var value = lines[i].replace("#", "");
         Log("INFO", value);
         $("#bureau_select").append($("<optgroup />").attr("label", value)); 
       }
+      // jinak se použije jako option
       else {
         var pair = lines[i].split("|");  
         Log("INFO", pair);
@@ -71,45 +79,54 @@ function LoadTemplatesList(data) {
     }
   }
 
+  // Dropdown select menu změň na vyhledávací pomocí knihovny Select2
   $(document).ready(function() {
     $("#bureau_select").select2();
   });
 }
 
+// Vymaže obsah ankety a vrátí zobrazení panelů na default
 function ResetForm() {
-  $("#poll_title").val("");
-  $("#poll_option_text").val("");
-  $("#poll_max_options").val("1");
-  $("#poll_length").val("0");
-  $("#poll_vote_change").prop('checked', false);
-  $("#poll_show_results").prop('checked', false);
-  $("li#options-panel-tab.tab").addClass("activetab");
-  $("li#attach-panel-tab.tab").removeClass("activetab");
+  // vrať pole ankety do původního stavu
+  $("#poll_title").val(""); // předmět ankety = ""
+  $("#poll_option_text").val(""); // možnosti ankety = ""
+  $("#poll_max_options").val("1"); // maximální počet možností v anketě = 1
+  $("#poll_length").val("0"); // délka ankety = 0
+  $("#poll_vote_change").prop('checked', false); // odškrtni možnost změny hlasu
+  $("#poll_show_results").prop('checked', false); // odškrtni zobrazení výsledků ankety
+  
+  // skryj panel ankety
   $("li#poll-panel-tab.tab").removeClass("activetab");
-  $("#options-panel").css("display", "block");
-  $("#attach-panel").css("display", "none");
   $("#poll-panel").css("display", "none");
+  
+  // skryj panel přílohy
+  $("li#attach-panel-tab.tab").removeClass("activetab");
+  $("#attach-panel").css("display", "none");
+  
+  // zobraz panel "možnosti příspěvku"
+  $("li#options-panel-tab.tab").addClass("activetab");
+  $("#options-panel").css("display", "block");
 }
 
+
+// Naplň formulář pro nový příspěvek obsahem šablony
 function FillPostingboxWithTemplate(data) {
+  // Odebereme anketu a resetujeme panely
   ResetForm();
 
+  // Textový soubor zprasujeme jako XML
   var xmlDoc = $.parseXML( data ),
   xml = $( xmlDoc );
-  $title = xml.find("title" );
-  
-  var lines = data.split("\n");
 
-
+  // pokud XML obsahuje element <sablona>, použijeme tuto logiku
   if(xml.find("sablona").length) {
-     // formulare
-     $("#bureau_templates_form").remove();
-
-     if(xml.find("formular").length) {
-      ComposeFormBlock(xml.find("formular"));
+     // inteligentní formuláře
+     $("#bureau_templates_form").remove(); // skryj panel z minula
+     if(xml.find("formular").length) { // pokud XML obsahuje element <formular>, přidej inteligentní formulář na stránku
+      ComposeFormBlock(xml.find("formular")); // jako argument odešli pouze subset XML souboru v elementu <formular>
      }
 
-     // anketa
+     // pokud XML obsahuje element <anketa>, vyplň hodnoty ankety
      if(xml.find("anketa").length) {
        var anketa = xml.find("anketa");
        FillWithText("#poll_title", "otazka", anketa);
@@ -119,6 +136,7 @@ function FillPostingboxWithTemplate(data) {
        FillWithBool("#poll_vote_change", "povolit-zmenu-hlasu", anketa);
        FillWithBool("#poll_show_results", "zobrazit-prubezne-vysledky", anketa);
        
+       // zobraz panel ankety
        $("li#options-panel-tab.tab").removeClass("activetab");
        $("li#attach-panel-tab.tab").removeClass("activetab");
        $("li#poll-panel-tab.tab").addClass("activetab");
@@ -126,71 +144,84 @@ function FillPostingboxWithTemplate(data) {
        $("#attach-panel").css("display", "none");
        $("#poll-panel").css("display", "block");
      }
+    // Pokud XML neobsahuje element <anketa>, odebereme anketu a resetujeme panely
     else {
        ResetForm();
     }
     
     // obsah
-     FillWithText("#postingbox #subject", "predmet", xml);
-     FillWithText("#postingbox textarea", "obsah", xml);
+     FillWithText("#postingbox #subject", "predmet", xml); // vyplníme předmět hodnotou elementu <predmet> z XML
+     FillWithText("#postingbox textarea", "obsah", xml); // vyplníme obsah příspěvku hodnotou elementu <obsah> z XML
   }
+  // pokud soubor není XML nebo neobsahuje pole <sablona>
   else {
-     $("#postingbox #subject").val(lines[0]); 
-     $("#postingbox textarea").val(lines.join("\n"));
+     $("#postingbox #subject").val(lines[0]);  // předmět příspěvku naplň prvním řádkem ze souboru
+     $("#postingbox textarea").val(lines.join("\n")); // obsah příspěvku naplň celý obsahem souboru
      Log("INFO", lines)
   }
 }
 
+// Vytvoř blok s inteligentním formulářem
 function ComposeFormBlock(formular) {
+  // nové prvky
   var block = $("<div></div>");
   var button = $("<input />");
 
+  // přidáme ID pro blok kvůli CSS stylům
   block.attr("id", "bureau_templates_form");
 
+  // z XML souboru vyber všechny elementy <pole>
   formular.find("pole").each(function() {
-    var formBox = inputRow.clone();
-    var formInput = formBox.find("input");
+    var formBox = inputRow.clone(); // naklonujeme si řádek "předmět"
+    var formInput = formBox.find("input"); // uložíme si i referenci na textové pole klonovaného řádku
     
-    formBox.find("label").text($( this ).text());
+    formBox.find("label").text($( this ).text()); // Text "předmět" přepíšeme na hodnotu z XML souboru
     
-    formInput.attr("type", $( this ).attr("typ"));
-    formInput.attr("name", $( this ).text());
-    formInput.attr("id", "bureau_template_form_" + $( this ).text());
-    formInput.addClass("bureau_template_form_input");
-    formInput.val("");
+    formInput.attr("type", $( this ).attr("typ")); // změníme typ textového pole na požadovaný typ definovaný v XML
+    formInput.attr("name", $( this ).text()); // jako jméno input pole použijeme jeho název
+    formInput.attr("id", "bureau_template_form_" + $( this ).text()); // přidáme i unikátní ID
+    formInput.addClass("bureau_template_form_input"); // přidáme class pro styl v CSS
+    formInput.val(""); // hodnotu input políčka nastavíme na prázdnou
 
+    // pokud je v XML u elementu <pole> definován parametr format="", vyplň jej jako hodnotu input pole
     var attr = $( this ).attr("format");
     if (typeof attr !== typeof undefined && attr !== false) {
       formInput.val($( this ).attr("format"));
-      formInput.attr("title", $( this ).attr("format"));
+      formInput.attr("title", $( this ).attr("format")); // přidej formát i jako title pro pole
     }
 
+    // připoj blok formuláře do nového bloku
     block.append(formBox);
   });
 
+  // Přidáme čudlik na naplnění šablony hodnotami pomocí OnClick akce
   button.attr("type", "button");
   button.val("Naplnit šablonu hodnotami");
   button.on("click", function() {
     InjectTemplate()
   });
 
+  // Do bloku pod inteligentní formulář přidáme tento čudlik
   block.append(button);
-  block.append($("<hr />"));
+  block.append($("<hr />")); // a oddělíme čárou
 
-  $("#bureau_select").parent().append(block);
+  $("#bureau_select").parent().append(block); // celý block přidáme za dropdown se seznamem šablon
 }
 
-function InjectTemplate() {
+// Naplň šablonu hodnotami z inteligentního formuláře
+function InjectTemplate() { 
+  // projdi každý input prvek z inteligentního formuláře
   $("#bureau_templates_form input.bureau_template_form_input").each(function() {
+    // pokud jeho hodnota není prázdná
     if($( this ).val() != "") {
-      var postingTextarea = $("#postingbox textarea");
+      var postingTextarea = $("#postingbox textarea"); // element obsahu příspěvku
   
-      // nahrad vsechny placeholdery za hodnoty z formulare
+      // nahraď všechny placeholdery za hodnoty z formuláře
       $("#postingbox #subject").val($("#postingbox #subject").val().replaceAll("{" + $( this ).attr("name") + "}", $( this ).val()));
       postingTextarea.val(postingTextarea.val().replaceAll("{" + $( this ).attr("name") + "}", $( this ).val()));
 
-      // nahrad vsechny placeholdery za hodnotu z formulare pro pouziti v url
-      var sanitizedValue = $( this ).val().replace(/[^\w\s]/gi, '_');
+      // nahraď všechny placeholdery s modifikátorem :rul za hodnoty z formuláře
+      var sanitizedValue = $( this ).val().replace(/[^\w\s]/gi, '_'); // odfiltruj nebezpečné znaky
       postingTextarea.val(postingTextarea.val().replaceAll("{" + $( this ).attr("name") + ":url}", sanitizedValue));
     }
   });
@@ -198,6 +229,7 @@ function InjectTemplate() {
 
 
 /* HELPERS */
+// Vyplň hodnotu elementu hodnotou z XML souboru (výsledný text je víceřádkový, skládá se z hodnot několika prvků z XML souboru spojených symbolem pro nový řádek)
 function FillWithMultilineText(element, tag, lines) {
   if(lines.find(tag).length) {
     lines.find(tag).each(function() {
@@ -207,6 +239,7 @@ function FillWithMultilineText(element, tag, lines) {
   }
 }
 
+// Vyplň hodnotu elementu hodnotou z XML souboru (text je jednořádkový)
 function FillWithText(element, tag, lines) {
   if(lines.find(tag).length) {
     $(element).val(lines.find(tag).text());
@@ -214,6 +247,7 @@ function FillWithText(element, tag, lines) {
   }
 }
 
+// Vyplň hodnotu elementu hodnotou z XML souboru (text je číslo, prázdný string nahraď 0)
 function FillWithNumber(element, tag, lines) {
   if(lines.find(tag).length) {
     var value = lines.find(tag).text();
@@ -227,6 +261,7 @@ function FillWithNumber(element, tag, lines) {
   }
 }
 
+// Zaškrtni/odškrtni element hodnotou z XML souboru (ano = checked, ne = unchecked)
 function FillWithBool(element, tag, lines) {
   if(lines.find(tag).length) {
     var value = lines.find(tag).text();
@@ -244,19 +279,23 @@ function FillWithBool(element, tag, lines) {
 
 
 /* INTERNAL FUNCTIONS */
+// Nahraď všechny výskyty substringu ve stringu
 String.prototype.replaceAll = function(search, replacement) {
   var target = this;
   return target.replace(new RegExp(search, 'g'), replacement);
 };
 
+// Funkce pro korektní logování - DATUM
 Date.prototype.today = function () { 
     return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
 }
 
+// Funkce pro korektní logování - ČAS
 Date.prototype.timeNow = function () {
      return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 }
 
+// Funkce pro korektní logování do konzole prohlížeče
 function Log(level, message) {
   if(DEBUG) {
     var newDate = new Date();
